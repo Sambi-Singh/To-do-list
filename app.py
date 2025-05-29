fbfc0d950bff094ed6eb2d003d061ff22b96f69b
Sambi <sambisingh289@gmail.com>
2025-05-29 10:01:03 -0500

    Insert tasks now

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-
+tasks = []
 #Update
 def changeTask():
     pass
 
 #Read
 def showTasks():
-    pass
+    for task in tasks:
+        print(f"{task}\n")
 
 #Delete
 def deleteTask():
@@ -13,13 +14,31 @@ def deleteTask():
 
 #Create
 def makeTask():
-    print("Please enter task name")
+    taskName = input("Please enter task name: ")
+    tasks.append(taskName)
+    if taskName in tasks:
+        print(f"'{taskName}' has been successfully added!")
+    else:
+        print("ERROR: Task could not be added at this time, please try again")
+
+
 
 def main():
     action = input('i - Inserts 1 task\nd - Deletes 1 task\nm- Modifies 1 existing task\n')
-    print(action)
+    #print(action)
+    if action.lower() == 'i':
+        makeTask()
+        showTasks()
+    elif action.lower() == 'd':
+        pass
+    elif action.lower() == 'm':
+        pass
+    else:
+        print("\nTest test!")
+
+
     
-    makeTask()
+    
 
 if __name__ == "__main__":
     main()
